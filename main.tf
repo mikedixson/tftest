72e32c91c5cb9921c755d22d34c17481bc877b84
Mike Dixson <mike-github.com@webheat.co.uk>
2024-11-05 23:57:30 +0000

    test file 2

--- main.tf
+++ main.tf
@@ -10,3 +10,8 @@ resource "local_file" "file1" {
     content  = "Hello World!"
     filename = "hello.txt"
 }
+
+resource "local_file" "file2" {
+    content  = "Hello World 2!"
+    filename = "hello.2txt"
+}
