8c162ad3f7a57669cc239b6c97f38c00483ae3f8
Mike Dixson <mike-github.com@webheat.co.uk>
2024-11-05 23:47:52 +0000

    added 2nd null resource and local file

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
 resource "null_resource" "test1" {
     
 }
+
+resource "null_resource" "test2 {
+    
+}
+
+resource "local_file" "file1" {
+    content  = "Hello World!"
+    filename = "hello.txt" 
+}
